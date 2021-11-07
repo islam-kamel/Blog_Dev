d7a6ae37dd3aac960bc3685903f7a0cfe769a203
islam kamel <islam.kamel@agr.svu.edu.eg>
2021-11-07 23:23:36 +0200

    Add Some Comments in Models File

--- blog_api/models.py
+++ blog_api/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 
 
 class Tags(models.Model):
+    """
+    Tags Table As Foreignkey Relationship
+    """
     type = models.CharField(max_length=100, help_text='Enter Post Tags Ex. Python, React.js')
 
     def __str__(self):
@@ -11,6 +14,9 @@ class Tags(models.Model):
 
 
 class Posts(models.Model):
+    """
+    Posts Table with include Tags Table
+    """
     post_title = models.CharField(max_length=250)
     post_content = models.TextField()
     created_data = models.DateTimeField(auto_now_add=timezone.now())
@@ -22,10 +28,13 @@ class Posts(models.Model):
 
 
 class Comments(models.Model):
+    """
+    Comments Table As Foreignkey Relationship with Posts Table, Current User
+    """
     comment = models.TextField()
     add_date = models.DateTimeField(auto_now_add=timezone.now())
     add_by = models.ForeignKey(User, on_delete=models.CASCADE)
     post_id = models.ForeignKey(Posts, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.comment[:20]} - {User.objects.get(pk=self.add_by.pk)}'
+        return f'{self.comment[:20]} - {User.objects.get(pk=self.add_by.pk)} - {Posts.objects.get(pk=self.post_id.pk)}'
