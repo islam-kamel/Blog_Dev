3fd74b50f51828afc56f83010ba323bca5461dda
islam kamel <islam.kamel@agr.svu.edu.eg>
2021-11-07 20:48:58 +0200

    Create Posts Table And Comments, Tags Table

--- blog_api/models.py
+++ blog_api/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Tags(models.Model):
+    type = models.CharField(max_length=100, help_text='Enter Post Tags Ex. Python, React.js')
+
+
+class Comments(models.Model):
+    comment = models.TextField()
+    add_date = models.DateTimeField(auto_now_add=timezone.now())
+    add_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Posts(models.Model):
+    post_title = models.CharField(max_length=250)
+    post_content = models.TextField()
+    created_data = models.DateTimeField(auto_now_add=timezone.now())
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ForeignKey(Tags, on_delete=models.PROTECT)
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
